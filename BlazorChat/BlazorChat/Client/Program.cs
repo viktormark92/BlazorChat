15045dc005ed19fa2ae6deb638b539c9a647bdf7
Tóth Viktor Márk <viktormark92@gmail.com>
2021-06-20 19:11:21 +0200

    EP04: - Cleared project, added third party razor component and used the component in client

--- BlazorChat/BlazorChat/Client/Program.cs
+++ BlazorChat/BlazorChat/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace BlazorChat.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredToast();
 
             await builder.Build().RunAsync();
         }
---
+++ BlazorChat/BlazorChat/Shared/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorChat.Shared
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(int contactId, string firstName, string lastName)
+        {
+            this.ContactId = contactId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
